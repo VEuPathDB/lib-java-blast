545d5df29d85af1452be8cab14d460f3093c557f
Elizabeth <foxcapade@gmail.com>
2022-03-12 23:55:13 -0500

    add defaults

--- src/main/kotlin/org/veupathdb/lib/blast/Blast.kt
+++ src/main/kotlin/org/veupathdb/lib/blast/Blast.kt
@@ -1,5 +1,25 @@
 package org.veupathdb.lib.blast
 
+import com.fasterxml.jackson.databind.node.ObjectNode
+import org.veupathdb.lib.blast.blastn.BlastN
+import org.veupathdb.lib.blast.blastn.BlastNImpl
+
 object Blast {
+
+  /**
+   * Creates a new [BlastN] instance by parsing the given JSON input.
+   *
+   * @param js JSON object to parse.
+   *
+   * @return The new [BlastN] instance.
+   */
+  fun blastn(js: ObjectNode): BlastN = BlastNImpl(js)
+
+  /**
+   * Creates a new, defaulted [BlastN] instance.
+   *
+   * @return The new [BlastN] instance.
+   */
+  fun blastn(): BlastN = BlastNImpl()
 
 }
