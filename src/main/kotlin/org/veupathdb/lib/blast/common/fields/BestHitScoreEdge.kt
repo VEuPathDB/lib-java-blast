bd807697c304d25af4d1b1890f6f1a24b46a58b7
Elizabeth <foxcapade@gmail.com>
2022-04-02 07:59:53 -0400

    -best_hit_score_edge

--- src/main/kotlin/org/veupathdb/lib/blast/common/fields/BestHitScoreEdge.kt
+++ src/main/kotlin/org/veupathdb/lib/blast/common/fields/BestHitScoreEdge.kt
@@ -3,22 +3,31 @@ package org.veupathdb.lib.blast.common.fields
 import com.fasterxml.jackson.databind.node.ObjectNode
 import org.veupathdb.lib.blast.common.FlagBestHitScoreEdge
 import org.veupathdb.lib.blast.serial.BlastField
+import org.veupathdb.lib.blast.util.*
 import org.veupathdb.lib.blast.util.add
 import org.veupathdb.lib.blast.util.append
+import org.veupathdb.lib.blast.util.optDub
 import org.veupathdb.lib.blast.util.put
 
 
+/**
+ * Default Value
+ */
 private const val Def = -1.0
 
+/**
+ * Exclusive Minimum
+ */
+private const val EMin = 0.0
 
-internal fun ParseBestHitScoreEdge(js: ObjectNode): BestHitScoreEdge {
-  val tmp = js[FlagBestHitScoreEdge] ?: return BestHitScoreEdge()
+/**
+ * Exclusive Maximum
+ */
+private const val EMax = 0.5
 
-  if (!tmp.isNumber)
-    throw IllegalArgumentException("$FlagBestHitScoreEdge must be a numeric value > 0 and < 0.5")
 
-  return BestHitScoreEdge(js.doubleValue())
-}
+internal fun ParseBestHitScoreEdge(js: ObjectNode) =
+  js.optDub(FlagBestHitScoreEdge) { BestHitScoreEdge(it)} ?: BestHitScoreEdge()
 
 
 /**
@@ -29,8 +38,8 @@ internal fun ParseBestHitScoreEdge(js: ObjectNode): BestHitScoreEdge {
 @JvmInline
 value class BestHitScoreEdge(val value: Double = Def) : BlastField {
   init {
-    if (value != Def && (value <= 0 || value >= 0.5))
-      throw IllegalArgumentException("$FlagBestHitScoreEdge must be (>0 and <0.5)")
+    if (value != Def)
+      value.inESet(FlagBestHitScoreEdge, EMin, EMax)
   }
 
   override val isDefault get() = value == Def
